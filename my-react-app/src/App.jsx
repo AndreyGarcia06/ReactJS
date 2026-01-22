3b6271d0361f5d823e13cc16fdaa4dc5883b4ebc
Andrey Ortega Garcia <240370@utxicotepec.edu.mx>
2026-01-22 14:19:18 -0600

    update: Se actualizó el codigo en app.jsx con componentes que listan

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,63 @@
 function app () {
+  
   return (
     <div>
       <h1> 5A EVND </h1>
       <h2> Profesor: </h2>
       <h3> M.T.I Ricardo Luna Santos </h3>
-      <h3> Alumnos: </h3>
-      <h4> Diego</h4>
-      <h4> Diego </h4>
-      <h4> Monica</h4>
-      <h4> Paola </h4>
+      <UserComponent />
+      <ProfileComponent />
+      <FeedComponent />
     </div>
 )
+}
+
+function UserComponent() {
+  const nombre = 'Andrey';
+  const apellidos = 'Ortega Garcia';
+  const nombreCompleto = <h2>El nombre es: {nombre} y sus apellidos: {apellidos}</h2>
+  return <h1> User Component {nombreCompleto}</h1>
+}
+
+function ProfileComponent() {
+  const users = [
+    {id: 1, name: 'Diego', role: 'Web Developer'},
+    {id: 2, name: 'Andrea', role: 'Web Designer'},
+    {id: 3, name: 'Paola', role: 'Team Leader'},
+  ]
+  return (
+  <>
+  <p>Lista de los usarios del sistema</p>
+  <ul> {
+    users.map (function(user, index) {
+      return (
+        <li key = {index}>{user.name} es un {user.role}</li>
+      )
+    })}
+  </ul>
+  </>
+  )
+}
+
+function FeedComponent() {
+  const mats = [
+    {id: 1, mat: 'Block'},
+    {id: 2, mat: 'Arena'},
+    {id: 3, mat: 'Cemento'},
+    {id: 4, mat: 'Tabique'},
+  ]
+  return (
+    <>
+    <p>Materiales de material para construir material</p>
+    <ul> {
+      mats.map (function(mats, index) {
+        return (
+          <li key = {index}> {mats.mat} sirve para construir construcciones construidas con material de construcción </li>
+        )
+      })
+     } </ul>
+    </>
+  )
 }
 
 export default app
