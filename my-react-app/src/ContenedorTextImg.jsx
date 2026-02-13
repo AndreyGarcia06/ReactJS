c711ddb2f7c4be34bbf8a3e33b7358e479d87206
Andrey Ortega Garcia <240370@utxicotepec.edu.mx>
2026-02-13 14:12:02 -0600

    update: Se modificó el mapa de prueba por uno geolocalizador para la posición del usuario

--- my-react-app/src/ContenedorTextImg.jsx
+++ my-react-app/src/ContenedorTextImg.jsx
@@ -1,5 +1,6 @@
-import Mapa from "./Mapa.jsx";
+
 import './ContenedorTextImg.css';
+import MapaGeo from './MapaGeo.jsx';
 
 function ContenedorTextImg() {
     return (
@@ -14,7 +15,7 @@ function Textos () {
         <div>
             <h3 className = "TextoTitulo"> Criptomonedas </h3>
             <p className = "TextoNormal">Las criptomonedas son activos digitales descentralizados, ya que no están controladas ni respaldadas por ningún banco central, y sus intercambios no requieren de intermediarios. El control de las transacciones de criptodivisas depende de una base de datos descentralizada, normalmente una blockchain (cadena de bloques).</p>
-            <Mapa lat = {19.43317210135107} lng = {-99.1494250757089} nombre_sucursal = {"Sucursal Ciudad de México"}   />
+            <MapaGeo />
         </div>
     )
 }
